6ccd4c9d9b9cf2ac69fec829776820ebb9187d54
Adnan Al-Hmoud <adnanalhmoud@outlook.com>
2025-11-12 01:57:29 +0300

    created a datetime for current time

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import datetime
 class Logging_Functions:
     def __init__(self):
         self.categories = { "error": [], "info": [], "access": [] }
+    
+    def add_log_entry(self,category, message, level):
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S %m - %d- %Y")
+        print(f"Current Date and Time:{now}")
+
+        log_entry = (datetime, message, level)
+        
+
+
+    
+
+
+        
 
     
         
