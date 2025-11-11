31eeb0d95bd8203b706103bae5cc77d44f5519a0
Adnan Al-Hmoud <adnanalhmoud@outlook.com>
2025-11-12 01:23:48 +0300

    Created two classses: Secure log, and Logging Functions

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("SecureLog is ready!")
+def secure_log():
+    categories = {
+        "error" :[],
+        "info" : [],
+        "access": []
+    }
+
+
+class Logging_Functions:
+    def __init__(self,category, message, level):
+        self.category = category
+        self.message = message
+        self.level = level
+    
+    def add_log_entry(category, message, level):
+        if 
+        
+
+
+
+
+
+
+
+
+
+
+
